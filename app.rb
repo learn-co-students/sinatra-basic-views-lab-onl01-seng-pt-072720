70dfa3312ff4370faaaed3683971027851fbd8f0
danainjax <danainjax@gmail.com>
2020-10-19 13:13:36 -0400

    Done.

--- app.rb
+++ app.rb
@@ -1,6 +1,9 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
-
+    # In `app.rb`, you'll want to create a controller action that responds to a GET request and loads `index.erb` in the browser.
+    get '/' do
+        erb :index
+    end
 
 end
